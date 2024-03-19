3b5eff2287a84927d4fa8f9533c467d36ef09510
Win-ther <emil.winther@hotmail.com>
2024-03-19 14:28:42 +0100

    Initial commit

---
+++ src/main/java/se/iths/SpringbootGroupProject/SpringbootGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package se.iths.SpringbootGroupProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGroupProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/iths/SpringbootGroupProject/SpringbootGroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.SpringbootGroupProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGroupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
