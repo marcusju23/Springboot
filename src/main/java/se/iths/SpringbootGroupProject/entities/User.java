0785fc61638262d62640b3c7446446dad1a0ce75
Win-ther <emil.winther@hotmail.com>
2024-03-19 16:08:56 +0100

    Add user-entity.

---
+++ src/main/java/se/iths/SpringbootGroupProject/entities/User.java
@@ -0,0 +1,56 @@
+package se.iths.SpringbootGroupProject.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String userName;
+    @Lob
+    private Byte[] image;
+    private String name;
+    private String email;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
