0bcdfdf01f2c24085c9d9c2be961442cbc5702f4
Win-ther <emil.winther@hotmail.com>
2024-03-19 16:12:20 +0100

    Add Jpa-buddy generated hashCode and equals.

--- src/main/java/se/iths/SpringbootGroupProject/entities/User.java
+++ src/main/java/se/iths/SpringbootGroupProject/entities/User.java
@@ -1,6 +1,9 @@
 package se.iths.SpringbootGroupProject.entities;
 
 import jakarta.persistence.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Entity
 public class User {
@@ -52,5 +55,21 @@ public class User {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
