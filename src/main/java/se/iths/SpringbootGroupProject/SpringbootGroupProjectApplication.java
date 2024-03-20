65b56a7c3fe2ecf392ff7e27b66bd8433fe28146
altmannk <katherine.altmann@iths.se>
2024-03-20 15:49:57 +0100

    Add Message entity

--- src/main/java/se/iths/SpringbootGroupProject/SpringbootGroupProjectApplication.java
+++ src/main/java/se/iths/SpringbootGroupProject/SpringbootGroupProjectApplication.java
@@ -2,8 +2,10 @@ package se.iths.SpringbootGroupProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringbootGroupProjectApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/se/iths/SpringbootGroupProject/entities/Message.java
@@ -0,0 +1,64 @@
+package se.iths.SpringbootGroupProject.entities;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDate date;
+
+    private String title;
+    private String messageBody;
+    private boolean isPrivateMessage;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessageBody() {
+        return messageBody;
+    }
+
+    public void setMessageBody(String messageBody) {
+        this.messageBody = messageBody;
+    }
+
+    public boolean isPrivateMessage() {
+        return isPrivateMessage;
+    }
+
+    public void setPrivateMessage(boolean privateMessage) {
+        isPrivateMessage = privateMessage;
+    }
+
+}
