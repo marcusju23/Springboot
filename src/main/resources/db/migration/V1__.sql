08c04ec652bb4ae64e883d1022a69f7a91260055
Win-ther <emil.winther@hotmail.com>
2024-03-21 22:55:25 +0100

    Generated the V1 of our database schema using flyway. The tables for our entities will now be created if they do not exist.

---
+++ src/main/resources/db/migration/V1__.sql
@@ -0,0 +1,26 @@
+CREATE TABLE message
+(
+    id              BIGINT AUTO_INCREMENT NOT NULL,
+    date            date                  NULL,
+    title           VARCHAR(255)          NULL,
+    message_body    VARCHAR(255)          NULL,
+    private_message BIT(1)                NOT NULL,
+    user_id         BIGINT                NULL,
+    CONSTRAINT pk_message PRIMARY KEY (id)
+);
+
+CREATE TABLE user
+(
+    id         BIGINT AUTO_INCREMENT NOT NULL,
+    user_name  VARCHAR(255)          NULL,
+    first_name VARCHAR(255)          NULL,
+    last_name  VARCHAR(255)          NULL,
+    email      VARCHAR(255)          NULL,
+    CONSTRAINT pk_user PRIMARY KEY (id)
+);
+
+ALTER TABLE user
+    ADD CONSTRAINT uc_user_user_name UNIQUE (user_name);
+
+ALTER TABLE message
+    ADD CONSTRAINT FK_MESSAGE_ON_USER FOREIGN KEY (user_id) REFERENCES user (id);
