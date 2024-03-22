ffd80d8dbd677a2dc9c870ceab23f8eef4845916
Win-ther <emil.winther@hotmail.com>
2024-03-22 12:49:12 +0100

    Add Todo-list and first method for welcome page.

---
+++ src/main/java/se/iths/SpringbootGroupProject/controllers/WebController.java
@@ -0,0 +1,22 @@
+package se.iths.SpringbootGroupProject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/web")
+public class WebController {
+    /* ToDo:
+     * Startsida: publika meddelanden, inloggningsruta, språkalternativ, välkomstmeddelande
+     * Homepage efter inloggning: dina meddelanden, din profil: din bild, namn, efternamn,email, profilnamn, utloggningsruta, språkalternativ
+     * Sida för att se alla meddelanden: Se alla meddelanden privata som publika där du ska kunna redigera ditt meddelande. utloggningsruta, språkalternativ
+     *
+     * OM TID FINNS-> Alla användare sida: kunna se alla användare och söka på användare
+     */
+    @GetMapping("/welcome")
+    public String getWelcomePage(){
+        return "welcome";
+    }
+
+}
