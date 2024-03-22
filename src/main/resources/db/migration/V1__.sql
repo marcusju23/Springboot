22d74363fe171138064dd045ae8322122d2f35b2
Win-ther <emil.winther@hotmail.com>
2024-03-22 14:17:14 +0100

    Fix so that the image is part of the migration-file and our database will now be created with an image column for every user.

--- src/main/resources/db/migration/V1__.sql
+++ src/main/resources/db/migration/V1__.sql
@@ -13,6 +13,7 @@ CREATE TABLE user
 (
     id         BIGINT AUTO_INCREMENT NOT NULL,
     user_name  VARCHAR(255)          NULL,
+    image      BLOB                  NULL,
     first_name VARCHAR(255)          NULL,
     last_name  VARCHAR(255)          NULL,
     email      VARCHAR(255)          NULL,
