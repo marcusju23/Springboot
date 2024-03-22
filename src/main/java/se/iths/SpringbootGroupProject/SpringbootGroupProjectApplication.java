1524e75f63cfacb84f54f57c45c3c0658a9578fc
Marcus <marcus.olsson98@gmail.com>
2024-03-22 12:23:08 +0100

    Add EnableCaching annotation to enable caching

--- src/main/java/se/iths/SpringbootGroupProject/SpringbootGroupProjectApplication.java
+++ src/main/java/se/iths/SpringbootGroupProject/SpringbootGroupProjectApplication.java
@@ -2,10 +2,12 @@ package se.iths.SpringbootGroupProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class SpringbootGroupProjectApplication {
 
 	public static void main(String[] args) {
