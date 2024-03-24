b5cca0dd305c2d9ffd7811b330cd1ea83e715ca8
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-03-24 21:18:43 +0100

    Update returnvalues

--- src/main/java/se/iths/springbootgroupproject/repositories/MessageRepository.java
+++ src/main/java/se/iths/springbootgroupproject/repositories/MessageRepository.java
@@ -4,20 +4,21 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import se.iths.springbootgroupproject.dto.PublicMessageAndUsername;
 import se.iths.springbootgroupproject.entities.Message;
 
 import java.util.List;
 
 public interface MessageRepository extends ListCrudRepository<Message, Long> {
 
-    List<Message> findAllByPrivateMessageIsFalse();
+    List<PublicMessageAndUsername> findAllByPrivateMessageIsFalse();
 
     @Query("""
             update Message m set m.privateMessage = ?1 where m.id = ?2
             """)
     @Modifying
     @Transactional
-    String setMessagePrivacy(boolean isPrivate, Long id);
+    void setMessagePrivacy(boolean isPrivate, Long id);
 
     @Query("""
             update Message m set m.messageBody = ?1 where m.id = ?2
@@ -31,7 +32,7 @@ public interface MessageRepository extends ListCrudRepository<Message, Long> {
             """)
     @Modifying
     @Transactional
-    String editTitle(String updatedTitle, Long id);
+    void editTitle(String updatedTitle, Long id);
 
 
 }
