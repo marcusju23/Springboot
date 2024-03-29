035fb08675dabdd3bf20d3eec3954478d30401dd
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-03-29 10:55:48 +0100

    Add logged in endpoint /userprofile

--- src/main/java/se/iths/springbootgroupproject/controllers/WebController.java
+++ src/main/java/se/iths/springbootgroupproject/controllers/WebController.java
@@ -2,12 +2,16 @@ package se.iths.springbootgroupproject.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import se.iths.springbootgroupproject.dto.PublicMessageAndUsername;
+import se.iths.springbootgroupproject.entities.User;
+import se.iths.springbootgroupproject.repositories.UserRepository;
 import se.iths.springbootgroupproject.services.MessageService;
 import se.iths.springbootgroupproject.services.UserService;
 
@@ -25,10 +29,12 @@ public class WebController {
      */
     private final MessageService messageService;
     private final UserService userService;
+    private final UserRepository userRepository;
 
-    public WebController(MessageService messageService, UserService userService) {
+    public WebController(MessageService messageService, UserService userService,UserRepository userRepository) {
         this.messageService = messageService;
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/welcome")
@@ -45,4 +51,28 @@ public class WebController {
         return "welcome";
     }
 
+    @GetMapping("/hello")
+    public String userProfile (Model model, @AuthenticationPrincipal OAuth2User principal) {
+        /*
+            This is the endpoint users are pointed to after oauth2 login.
+            The path should probably change name to /users/profiles and access the oauth2User or /users/profiles/{uniqueValue} and fetch
+            uniqueValue from userService.
+         */
+
+        System.out.println("User LoginName " + principal.getAttribute("login"));
+
+        User user = userRepository.findByUserName(principal.getAttribute("login"));
+        System.out.println("USER FIRSTNAME " + user.getFirstName());
+
+        String userName = principal.getAttribute("login");
+        String profilePic = principal.getAttribute("avatar_url");
+        String name = principal.getAttribute("name");
+
+        System.out.println(principal.getAttribute("avatar_url").toString());
+        model.addAttribute("name" , name);
+        model.addAttribute("userName", userName);
+        model.addAttribute("profilepic",profilePic);
+        model.addAttribute("all", principal);
+        return "hello";
+    }
 }
