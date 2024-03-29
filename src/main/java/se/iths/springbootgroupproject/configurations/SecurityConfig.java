5bf9859da0357e3f52564e2f5e22f556a8c200ec
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-03-29 09:09:25 +0100

    Add securityconfig to authenticate and authorize oauth2 logins

---
+++ src/main/java/se/iths/springbootgroupproject/configurations/SecurityConfig.java
@@ -0,0 +1,54 @@
+package se.iths.springbootgroupproject.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.web.client.RestClient;
+import se.iths.springbootgroupproject.services.github.GithubOAuth2UserService;
+
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private GithubOAuth2UserService githubOAuth2UserService;
+
+    @Bean
+    SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/web/welcome", "/login", "/oauth/**", "/logout", "/error**").permitAll()
+                        .requestMatchers("/web/hello").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfoEndpoint ->
+                                userInfoEndpoint
+                                        .userService(githubOAuth2UserService))
+                        .successHandler(oauth2LoginSuccessHandler()));
+        return http.build();
+    }
+
+    @Bean
+    static RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+        hierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER\n" +
+                               "ROLE_USER > ROLE_GUEST");
+        return hierarchy;
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler oauth2LoginSuccessHandler() {
+        return new SimpleUrlAuthenticationSuccessHandler("/web/hello");
+    }
+
+    @Bean
+    RestClient restClient() {
+        return RestClient.create();
+    }
+}
