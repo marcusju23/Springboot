213d663e726e05e67b0fbddcd9a49ed1a8315796
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-04-05 11:24:26 +0200

    Add id as parameter

--- src/main/java/se/iths/springbootgroupproject/dto/MessageAndUsername.java
+++ src/main/java/se/iths/springbootgroupproject/dto/MessageAndUsername.java
@@ -3,5 +3,5 @@ package se.iths.springbootgroupproject.dto;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-public record MessageAndUsername(LocalDate date, String title, String messageBody, String userUserName) implements Serializable {
+public record MessageAndUsername(Long id, LocalDate date, String title, String messageBody, String userUserName) implements Serializable {
 }
